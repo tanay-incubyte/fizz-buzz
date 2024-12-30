0de3751e706e6ca3a0643c6676ddcdcdafd9cd58
tanay-incubyte <tanay@incubyte.co>
2024-12-30 11:38:54 +0530

    update fizz_buzz method to handle multiple of 3

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,3 +1,7 @@
 def fizz_buzz(number)
-	number
+	if number % 3 == 0
+		'Fizz'
+	else
+		number
+	end
 end
--- fizz_buzz_spec.rb
+++ fizz_buzz_spec.rb
@@ -4,9 +4,12 @@ RSpec.describe '#fizz_buzz' do
   it 'returns the correct integer' do
     expect(fizz_buzz(1)).to eq(1)
     expect(fizz_buzz(2)).to eq(2)
+    expect(fizz_buzz(4)).to eq(4)
   end
 
 	it 'returns "Fizz" for multiples of 3' do
 		expect(fizz_buzz(3)).to eq('Fizz')
+		expect(fizz_buzz(6)).to eq('Fizz')
+		expect(fizz_buzz(9)).to eq('Fizz')
 	end
 end
