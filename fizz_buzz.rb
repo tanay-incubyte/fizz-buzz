4c7c8bca0f312d3da184cf851ce2a225704b37c1
tanay-incubyte <tanay@incubyte.co>
2024-12-30 17:35:10 +0530

    optimized fizz_buzz method
    used string concatenation to combine the result with conditions

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,15 +1,8 @@
 def fizz_buzz(number)
-	fizz = (number % 3 == 0)
-	buzz = (number % 5 == 0)
-	if fizz && buzz
-		'FizzBuzz'
-	elsif fizz
-		'Fizz'
-	elsif buzz
-		'Buzz'
-	else
-		number
-	end
+  result = ''
+  result << 'Fizz' if number % 3 == 0
+  result << 'Buzz' if number % 5 == 0
+  result.empty? ? number : result
 end
 
 1.upto(15) do
