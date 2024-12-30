85da2b63104df678b49aa2d3dce9678c94cdd8ca
tanay-incubyte <tanay@incubyte.co>
2024-12-30 11:33:03 +0530

    update test with Fizz - multiple of 3

--- fizz_buzz_spec.rb
+++ fizz_buzz_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe '#fizz_buzz' do
   it 'returns the correct integer' do
     expect(fizz_buzz(1)).to eq(1)
     expect(fizz_buzz(2)).to eq(2)
-    expect(fizz_buzz(3)).to eq(3)
   end
+
+	it 'returns "Fizz" for multiples of 3' do
+		expect(fizz_buzz(3)).to eq('Fizz')
+	end
 end
